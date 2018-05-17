7605dfdf880138d659d392fcac7540dd6d67a657
higor1104 <higoraraujo94@gmail.com>
2018-05-17 20:54:25 -0300

    Mapeamento da entidade Playlist

--- src/main/java/com/handson/config/domain/model/Playlist.java
+++ src/main/java/com/handson/config/domain/model/Playlist.java
@@ -1,5 +1,17 @@
 package com.handson.config.domain.model;
 
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Playlist {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+	
+	private List<Music> musics;
 
 }
