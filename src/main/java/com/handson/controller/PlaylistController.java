90193da5a4aa03bc4204bc871ab93a5abd95c063
Matheus Amaro <matheus.amaro@gmail.com>
2018-05-17 21:10:51 -0300

    PlaylistService e PlaylistController mapeadas com a camada de serviço

--- src/main/java/com/handson/controller/PlaylistController.java
+++ src/main/java/com/handson/controller/PlaylistController.java
@@ -1,8 +1,29 @@
 package com.handson.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller("api/playlist")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.handson.config.domain.model.Playlist;
+import com.handson.config.domain.service.PlaylistService;
+
+@RestController
+@RequestMapping(path = "/api/playlists", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PlaylistController {
-
+	
+	@Autowired
+	private PlaylistService playlistService;
+	
+	public PlaylistController() {
+		super();
+	}
+	
+	@GetMapping("/")
+	public List<Playlist> findAll() {
+		return playlistService.findAll();
+	}
 }
